730ae27828b1c54f88f21892bad3eb21ef15907f
Allectona <miranos.hill@gmail.com>
2023-08-15 23:23:57 +0100

    Initial Commit (1-4 exercises)

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* 1 задание */
+
+        for (int i = 1; i<=10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
+
+        /* 2 задание */
+
+        for (int a = 10; a>=1; a--) {
+            System.out.println("Итерация цикла " + a);
+        }
+
+        /* 3 задание */
+
+        for (int b = 0; b<17; b=b+2) {
+            System.out.println("Итерация цикла " + b);
+        }
+
+        /* 4 задание */
+
+        for (int c = 10; c>=-10; c--) {
+            System.out.println("Итерация цикла " + c);
+        }
+    }
+}
